c030ad02cb39d0aa8ecd41a9de0085efbb5ebdc1
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-09-26 16:18:16 +0530

    Add CategoryCard

--- Frontend/src/components/UserDashboard.jsx
+++ Frontend/src/components/UserDashboard.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import Navbar from './Navbar'
+// ...existing code...
+import React from "react";
+import Navbar from "./Navbar";
+
+import categories from "../category.js";
+import CategoryCard from "./CategoryCard";
 
 const UserDashboard = () => {
   return (
-    <div><Navbar/>
-    </div>
-  )
-}
+    <div className="w-full h-full flex flex-col gap-5 items-center overflow-y-auto scrollbar-thin scrollbar-thumb-[#ff4d2d] scrollbar-track-transparent scroll-smooth">
+      <Navbar />
 
-export default UserDashboard
+      <div className="w-full max-w-6xl flex flex-col gap-5 justify-center  items-center">
+        <h1 className="text-3xl text-gray-800  sm:text-3xl">
+          Inspiration for your first order
+        </h1>
+
+        <div className="w-full p-2">
+          <div
+            className="rounded-3xl
+            grid grid-cols-1
+            cols-2-320 cols-3-425
+          md:flex gap-4 md:flex-nowrap items-center
+          lg:flex lg:flex-nowrap
+          overflow-x-auto pb-2 pl-4 pr-4 scrollbar-thin scrollbar-thumb-[#ff4d2d] scrollbar-track-transparent scroll-smooth"
+          >
+            {/* Category card container */}
+            {categories.map((cat, index) => (
+              <CategoryCard data={cat} key={cat.category ?? index} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
+// ...existing code...
