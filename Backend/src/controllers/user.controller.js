af72ef2901d17186c858efbd3991d1ee8ef90f73
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-10-08 18:58:26 +0530

    Add updateUserLocation endpoint: implement logic for updating user location with GeoJSON format

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { updateMany } = require("../models/order.model");
 const userModel = require("../models/user.model");
 
 
@@ -15,3 +16,30 @@ exports.getUsers = async (req, res) => {
     return res.status(500).json({ message: `get current user error ${err}` });
   }
 };
+
+
+exports.const = updateUserLocation = async (req, res) => {
+  try {
+    const { latitude, longitude } = req.body;
+    if (!latitude || !longitude) {
+      return res
+        .status(400)
+        .json({ message: "Please provide both latitude and longitude" });
+    }
+    // support both auth middleware shapes: req.user (object) or req.userId (id)
+    const user = await userModel.findByIdAndUpdate(req.userId || (req.user && req.user._id), {
+      Location: {
+      type: "Point",
+      coordinates: [longitude, latitude], // Note: GeoJSON format is [longitude, latitude]
+      }
+    },{new:true});
+
+    if (!user) {
+      return res.status(401).json({ messagee: "Please login first" });
+    }
+    return res.status(200).json({ message: "Location updated successfully", user });  
+    
+  } catch (err) {
+    
+  }
+}
