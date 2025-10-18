377f14e72514fdc27362f55c6ebc76938a7795bd
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-10-18 23:52:28 +0530

    Add available assignments feature to DeliveryBoy component: fetch and display available orders for delivery boys

--- Frontend/src/components/DeliveryBoy.jsx
+++ Frontend/src/components/DeliveryBoy.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect ,useState} from "react";
 import Navbar from "./Navbar";
 import { useSelector } from "react-redux";
 import axios from "axios";
@@ -7,6 +7,8 @@ import { useNavigate } from "react-router-dom";
 const DeliveryBoy = () => {
   const navigate = useNavigate();
   const userData = useSelector((state) => state?.user?.userData);
+  const [availableAssignments, setAvailableAssignments] = useState(null);
+   
 
   const getAssignments = async () => {
     if (!userData) return; // wait until auth is ready
@@ -14,6 +16,7 @@ const DeliveryBoy = () => {
       const response = await axios.get("/api/order/get-assigned-orders", {
         withCredentials: true,
       });
+      setAvailableAssignments(response.data);
       console.log("DeliveryBoy assignments:", response.data);
     } catch (err) {
       console.error(
@@ -31,8 +34,8 @@ const DeliveryBoy = () => {
   return (
     <div>
       <Navbar />
-      <div className="w-screen h-[80vw] flex items-center justify-center bg-gray-50 gap-2 overflow-y-auto">
-        <div className="bg-white rounded-2xl text-center shadow-md p-5 flex flex-col justify-start items-center w-[90%] border border-orange-100">
+      <div className="w-full flex items-center justify-center flex-col bg-gray-50 gap-2 overflow-y-auto mt-5">
+        <div className="bg-white rounded-2xl text-center shadow-md p-5 flex flex-col justify-start items-center w-[80%] border border-orange-100">
           <h1 className="text-xl font-bold text-[#ff4d2d] ">
             Welcome, {userData?.fullName}
           </h1>
@@ -42,6 +45,45 @@ const DeliveryBoy = () => {
             <span className="font-semibold"> longitude: </span>
             {userData?.location?.coordinates?.[0] ?? "N/A"}
           </p>
+        </div>
+
+        {/*  AvailableAssignments */}
+
+        <div className="bg-white rounded-2xl p-5 shadow-md w-[80%] border border-orange-100 ">
+          <h1 className="text-xl font-bold mb-4 flex items-center gap-2">
+            {" "}
+            Availble Orders{" "}
+          </h1>
+          <div className="space-y-4">
+            {availableAssignments && availableAssignments.length > 0 ? (
+              availableAssignments.map((assignment, index) => (
+                <div
+                  className="border rounded-lg p-4 flex justify-between items-center "
+                  key={index}
+                >
+                  <div>
+                    <p className="text-sm font-semibold">
+                      {assignment?.shopName}
+                    </p>
+                    <p className="text-sm text-gray-500">
+                      <span className="font-semibold">Delivery Address :</span>
+                      {assignment?.deliveryAddress.text}
+                    </p>
+                    <p className="text-xs text-gray-400">
+                      {assignment.items.length}
+                      items | ₹{assignment?.subtotal}
+                    </p>
+                  </div>
+
+                  <button className='bg-orange-500 text-white px-4 py-1 rounded-lg text-sm hover:bg-orange-600 '>Accept</button>
+                </div>
+              ))
+            ) : (
+              <p className="text-gray-500 ">
+                No available orders at the moment.
+              </p>
+            )}
+          </div>
         </div>
       </div>
     </div>
