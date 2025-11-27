3fbeb2b8718aa88b55047aed56ef4c207eb676a8
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 00:20:38 +0530

    Add video controller: implement getAllVideos function to retrieve video data

---
+++ Backend/src/controllers/video.controller.js
@@ -0,0 +1,14 @@
+const ItemImport = require("../models/item.model");
+const itemModel = ItemImport.default || ItemImrt;
+
+// Get all videos
+exports.getAllVideos = async (req, res) => {
+  try {
+    const videos = await itemModel
+      .find({ video: { $exists: true } })
+      .select("video videoTitle videoDescription -_id");
+    return res.status(200).json({ videos });
+  } catch (error) {
+    return res.status(500).json({ message: `Error getting videos: ${error}` });
+  }
+};
