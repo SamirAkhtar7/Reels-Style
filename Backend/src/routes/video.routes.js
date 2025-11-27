da67c943b3b738c5b73e8e650dd8b39f349dce2a
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 00:20:13 +0530

    Add video routes: implement public route for fetching all videos

---
+++ Backend/src/routes/video.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const { getAllVideos } = require("../controllers/video.controller");
+
+// Public Routes
+
+router.get("/get-all-videos", getAllVideos);
+
+// Protected Routes
+
+module.exports = router;
