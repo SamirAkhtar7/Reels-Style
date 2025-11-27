c6178cb8511350256d35264ec28c90a0ba644ce7
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 00:49:52 +0530

    Refactor video routes: organize imports and enhance route for fetching videos by shop ID

--- Backend/src/routes/video.routes.js
+++ Backend/src/routes/video.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 
-const { getAllVideos } = require("../controllers/video.controller");
+const { getAllVideos} = require("../controllers/video.controller");
+const { getAllVideoByShopeId} = require("../controllers/video.controller"); 
 
 // Public Routes
 
 router.get("/get-all-videos", getAllVideos);
+router.get("/get-all-videos-by-shop/:shopId",getAllVideoByShopeId  );
 
 // Protected Routes
 
