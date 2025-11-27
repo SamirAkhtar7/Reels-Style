1339fad1146f76b002a682ea50d3a2ff94d09a30
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 00:49:33 +0530

    Add video controller: implement getAllVideoByShopeId function to retrieve videos by shop ID

--- Backend/src/controllers/video.controller.js
+++ Backend/src/controllers/video.controller.js
@@ -12,3 +12,18 @@ exports.getAllVideos = async (req, res) => {
     return res.status(500).json({ message: `Error getting videos: ${error}` });
   }
 };
+
+exports.getAllVideoByShopeId = async (req, res) => {
+
+    const { shopId } = req.params;
+
+    try {
+        const videos = await itemModel
+            .find({ shop: shopId, video: { $exists: true } })
+            .select("video videoTitle videoDescription -_id");
+        return res.status(200).json({ videos });
+    } catch (error) {
+        return res.status(500).json({ message: `Error getting videos: ${error}` });
+    }
+   
+}
