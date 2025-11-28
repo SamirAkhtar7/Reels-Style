345a8928e5ab5ee21eb59b8cfd3c70028c34bfb7
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 23:04:50 +0530

    Add VideoCard component: initial implementation for video display

---
+++ Frontend/src/components/VideoCard.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+
+const VideoCard = ({video})=>{
+    return(
+        <div>
+     <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+    hellooo
+  
+
+
+     </div>
+        </div>
+    )
+}
+
+export default VideoCard;
