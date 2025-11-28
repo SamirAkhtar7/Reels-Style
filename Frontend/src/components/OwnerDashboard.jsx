7e37dcab2c05c9354e7cffa1fdf7b9fc9d7e51ae
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 18:59:06 +0530

    Add video fetching and toggle functionality in OwnerDashboard: implement getFoodVideo function and buttons to switch between food items and videos

--- Frontend/src/components/OwnerDashboard.jsx
+++ Frontend/src/components/OwnerDashboard.jsx
@@ -5,13 +5,41 @@ import { FaUtensils } from "react-icons/fa6";
 import { FaPen } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import OwnerItemCard from "./OwnerItemCard";
+import { useState } from "react";
+import axios from "../config/axios";
 
 const OwnerDashboard = () => {
   const navigate = useNavigate();
   const myShopData = useSelector((state) => state.owner?.myShopData);
+const [showItems, setShowItems] = useState(true);
+const [showVideos, setShowVideos] = useState(false);
+
+
 
 //console.log("OwnerDashboard myShopData items:", myShopData);
 
+const getFoodVideo= async()=>{
+  try{
+    const response = await axios(
+      `/api/video/get-all-videos-by-shop/${myShopData._id}`,
+      {
+        withCredentials: true,
+      }
+    );
+  console.log("Food Videos:", response.data.videos);
+  }
+  catch(err){
+    console.error("Error fetching food videos:", err);
+  }
+
+}
+
+getFoodVideo();
+// useEffect(() => {
+//   if(showVideos){
+//     getFoodVideo();
+//   }
+// }, [showVideos]);    
 
   // console.log("OwnerDashboard myShopData:", myShopData);
   return (
@@ -76,6 +104,33 @@ const OwnerDashboard = () => {
             </div>
           </div>
 
+          <div className="w-full flex justify-center gap-4 mt-8">
+            <div 
+            className="">
+              <button
+                onClick={() => {
+                  setShowItems(true);
+                  setShowVideos(false);
+                }}
+               className="bg-[#ff4d2d] text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-600 transition-colors duration-200">
+                {" "}
+                Food Items
+              </button>
+            </div>
+            <div>
+              <button 
+                onClick={() => {
+                  setShowItems(false);
+                  setShowVideos(true);
+                }}
+              
+              className="bg-[#ff4d2d] text-white px-5 sm:px-6 py-2 rounded-full font-medium shadow-md hover:bg-orange-600 transition-colors duration-200">
+                {" "}
+                Food Videos
+              </button>
+            </div>
+          </div>
+
           {myShopData?.items?.length === 0 && (
             <div className="flex justify-center items-center p-4 sm:p-6">
               <div className="w-full justify-center max-w-md bg-white shadow-xl rounded-2xl p-6 border border-gray-100 hover:shadow-xl transition-shadow duration-300">
@@ -103,15 +158,17 @@ const OwnerDashboard = () => {
         </>
       )}
 
-      {myShopData?.items?.length > 0 && (
-        <div className="mt-10  max-w-6xl min-w-[300px] p-2 mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 p-1 lg:grid-cols-3 gap-4">
-            {myShopData.items.map((item) => (
-              <OwnerItemCard data={item} key={item._id} />
-            ))}
+      {/* {myShopData?.items?.length > 0 && ( */}
+      {myShopData?.items?.length > 0 &&
+        (showItems && (
+          <div className="mt-10  max-w-6xl min-w-[300px] p-2 mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-2 p-1 lg:grid-cols-3 gap-4">
+              {myShopData.items.map((item) => (
+                <OwnerItemCard data={item} key={item._id} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        ))}
       {/* <OwnerItemCard data={myShopData?.items} /> */}
     </div>
   );
