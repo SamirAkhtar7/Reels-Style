73a329c9be94a6ee7425cca2bc235d995c881ad8
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-28 18:58:26 +0530

    Refactor food item model: rename schema and fields to align with video data structure

--- Backend/src/models/foodItem.model.js
+++ Backend/src/models/foodItem.model.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 const userModel = require ("./user.model")
 
-const foodSchema = new mongoose.Schema({
-  name: {
+const videoSchema = new mongoose.Schema({
+ name: {
     type: String,
     require: true,
   },
-  video: {
+  videoUrl: {
     type: String,
     require: true,
   },
   description: {
     type: String,
  
-    },
+  },
+  like: {
+    type: Number,
+    default: 0,
+  }
 });
 
-const foodModel = mongoose.model("food", foodSchema)
-module.exports = foodModel;
+const videoModel = mongoose.model("food", videoSchema)
+module.exports = videoModel;
