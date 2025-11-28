e0566933b5d0263e5b3ba4aad248f63fc2ae18c9
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-29 00:41:43 +0530

    Fix import case sensitivity for UserDashboard component in Home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import UserDashboard from "../components/userDashboard";
+import UserDashboard from "../components/UserDashboard";
 import OwnerDashboard from "../components/OwnerDashboard";
 import DeliveryBoy from "../components/DeliveryBoy";
 
 const Home = () => {
-   const slice = useSelector((state) => state.user);
+  const slice = useSelector((state) => state.user);
 
   const user = slice?.user ?? slice?.userData ?? slice?.userDate ?? null;
 
-
-
   return (
     <div className="w-screen h-screen flex  justify-center  bg-[#fff9f6]">
-          {user?.role === "user" && <UserDashboard />
+      {
+        user?.role === "user" && <UserDashboard />
         //       : (
         // <h1 className="text-2xl font-medium">Welcome</h1>
         //   )
-        }
-          
-          {user?.role === "owner" && <OwnerDashboard />}
-            {user?.role === "foodDelivery" && <DeliveryBoy />}
+      }
+
+      {user?.role === "owner" && <OwnerDashboard />}
+      {user?.role === "foodDelivery" && <DeliveryBoy />}
     </div>
   );
 };
