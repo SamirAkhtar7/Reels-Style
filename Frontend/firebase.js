b26bf52641eb2feb70357020e32ce584fd163633
SamirAkhat7 <samir.akhtar444@gmail.com>
2025-11-29 02:55:36 +0530

    Update Firebase configuration: correct authDomain and projectId for consistency

--- Frontend/firebase.js
+++ Frontend/firebase.js
@@ -9,12 +9,15 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain:
     import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
-    "reels-food-delivery.firebaseapp.com",
-  projectId: "reels-food-delivery",
-  storageBucket: "reels-food-delivery.firebasestorage.app",
+    "reels-style-delivery.firebaseapp.com",
+  projectId: "reels-style-delivery",
+  storageBucket: "reels-style-delivery.firebasestorage.app",
   messagingSenderId: "914222338094",
   appId: "1:914222338094:web:87963e41ca366675b64fbf",
 };
+console.log(">>> CURRENT HOST:", window.location.origin);
+console.log(">>> FIREBASE AUTH DOMAIN:", firebaseConfig.authDomain);
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
